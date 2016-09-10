6c29473bfa4a1a2bf87ada8afe4e2ce2595ff87f
Shivam Gupta <shivam.gupta@olacabs.com>
2016-09-10 19:35:57 +0530

    Iteration 2 of website design by jarvas

--- index.js
+++ index.js
@@ -2,7 +2,7 @@
 *     File Name           :     index.js
 *     Created By          :     shivam.gupta
 *     Creation Date       :     [2016-09-09 16:19]
-*     Last Modified       :     [2016-09-10 03:48]
+*     Last Modified       :     [2016-09-10 16:11]
 *     Description         :      
 **********************************************************************************/
 
@@ -26,28 +26,20 @@ app.intent('webdesign', {
  'slots': {
      'COMMAND': 'MYCOMMAND',
      'MODIFIEE': 'MYMODIFIEE',
-     'PREVCOLOR': 'MYCOLOR',
-     'NEWCOLOR': 'MYCOLOR',
-     'PREVIMAGE': 'MYIMAGE',
-     'NEWIMAGE': 'MYIMAGE',
-     'FONT': 'MYFONT',
-     'SHIFT': 'NUMBER'
+     'PREVSTATE': 'AMAZON.LITERAL',
+     'UPDATESTATE': 'AMAZON.LITERAL'
    },
-  //'utterances': ['{-|COMMAND} {-|MODIFIEE} {from|} {PREVCOLOR|PREVIMAGE|} {to} {NEWCOLOR|NEWIMAGE|FONT|SHIFT}']
-  'utterances': ['{-|COMMAND} {-|MODIFIEE} {from|} {-|PREVCOLOR} {to} {-|NEWCOLOR}']
+  'utterances': ['{|please} {-|COMMAND} {|the} {-|MODIFIEE} {from|} {-|PREVSTATE} {to} {-|UPDATESTATE}']
 },
   function(req, res) {
     var command = req.slot('COMMAND');
     var modifiee = req.slot('MODIFIEE');
-    var prevColor = req.slot('PREVCOLOR');
-    var newColor = req.slot('NEWCOLOR');
-    var prevImage = req.slot('PREVIMAGE');
-    var newImage = req.slot('NEWIMAGE');
-    var font = req.slot('FONT');
-    var shift = req.slot('SHIFT');
+    var prevState = req.slot('PREVSTATE');
+    var updateState = req.slot('UPDATESTATE');
     var reprompt = 'Please tell me what to update like images, fonts, background etc. I have some other important works to finish';
     var webdesignHelper = new JarvasWebdesignHelper();
-    console.log("Request received " + command + " " + modifiee + " " + prevColor + " " + newColor);
+    var SUCCESSFUL_MESSAGE = " all hail jarvas";
+    console.log("Request received " + command + " " + modifiee + " " + prevState + " " + updateState);
     if (_.isEmpty(command)) {
       var prompt = 'I didn\'t hear proper command to do. Tell me properly what to do like update, shift etc otherwise you might loose this hackathon';
       res.say(prompt).reprompt(reprompt).shouldEndSession(false);
@@ -62,12 +54,12 @@ app.intent('webdesign', {
       return true;
     } */else {
       debugger;
-      webdesignHelper.updateWebsite(modifiee, prevColor, newColor).then(function(bodyResponse){
-          console.log("Update successful bolo jarvas baba ki jai");
-          res.say("Update successful bolo jarvas baba ki jai").send();
+      webdesignHelper.updateWebsite(modifiee, prevState, updateState).then(function(bodyResponse){
+          console.log(bodyResponse + SUCCESSFUL_MESSAGE);
+          res.say(bodyResponse + SUCCESSFUL_MESSAGE).send();
       }).catch(function(err) {
               console.log(err);
-              var prompt = 'I didn\'t have data for an airport code of ' + airportCode;
+              var prompt = 'I didn\'t hear proper command to do. Tell me properly what to do like update, shift etc otherwise you might loose this hackathon';
               res.say(prompt).reprompt(reprompt).shouldEndSession(false).send();
       });  
       console.log("Done with processing"); 
--- jarvas_webdesign_helper.js
+++ jarvas_webdesign_helper.js
@@ -2,7 +2,7 @@
 *     File Name           :     jarvas_webdesign_helper.js
 *     Created By          :     shivam.gupta
 *     Creation Date       :     [2016-09-09 13:44]
-*     Last Modified       :     [2016-09-10 04:17]
+*     Last Modified       :     [2016-09-10 16:08]
 *     rization: Bearer 7wbOTxM8l6X2Lqy7cFT(gRThK&Gr0CovQ9r2cBO9yiCXt5Tr#KTQ@hUGa2(Hfu^N' \
 *
 *     Description         :      
@@ -12,83 +12,46 @@
 
 var _ = require('lodash');
 var rp = require('request-promise');
-var ENDPOINT = 'http://services.faa.gov/airport/status/';
+var ENDPOINT = 'http://54.169.212.81:9050/api/updateWebsite';
 
 function JarvasWebdesignHelper() {}
 
-JarvasWebdesignHelper.prototype.requestAirportStatus = function(airportCode) {
-  return this.getAirportStatus(airportCode).then(
-    function(response) {
-          console.log('success - received airport info for ' + airportCode);
-          return response.body;
-        }
-  );
-};
-
-JarvasWebdesignHelper.prototype.updateWebsite = function(modifiee, prevColor, newColor) {
-  console.log('updating ' + modifiee + " from " + prevColor + " " + newColor);
+JarvasWebdesignHelper.prototype.updateWebsite = function(modifiee, prevState, updateState) {
+  console.log('updating ' + modifiee + " from " + prevState + " " + updateState);
   console.log("Returning from call");
-  var airportCode = "SFO";
-  return this.getAirportStatus(airportCode).then(
+  return this.updateWebsiteAPI(modifiee, updateState).then(
     function(response) {
-          console.log('success - received airport info for ' + airportCode);
-          return response.body;
+          console.log('success - requested to modify website for ' + JSON.stringify(response));
+          return response.body.message;
         }
   ).catch(function (err) {
          console.log('Error is ' + JSON.stringify(err));
+         return err.message;
   });
 };
 
-JarvasWebdesignHelper.prototype.requestNewBlog = function(airportCode) {
-  return this.createNewBlog(airportCode).then(
-    function(response) {
-          console.log('success - received airport info for ' + airportCode);
-          return response.body;
-        }
-  ).catch(function (err) {
-      console.log('Error is ' + JSON.stringify(err));
-  });
-};
-
-
-JarvasWebdesignHelper.prototype.getAirportStatus = function(airportCode) {
-   var options = {
-      method: 'GET',
-      uri: ENDPOINT + airportCode,
-      resolveWithFullResponse: true,
-      json: true
-    };
-  return rp(options);
-};
-
-JarvasWebdesignHelper.prototype.createNewBlog = function(airportCode) {
+JarvasWebdesignHelper.prototype.updateWebsiteAPI = function(action, updateState) {
   var options = {
       method: 'POST',
-      uri: 'https://public-api.wordpress.com/rest/v1.2/sites/82974409/posts/new/',
-      headers: {
-          'authorization': 'Bearer 7wbOTxM8l6X2Lqy7cFT(gRThK&Gr0CovQ9r2cBO9yiCXt5Tr#KTQ@hUGa2(Hfu^N',
-          'User-Agent': 'Request-Promise'
-      },
+      uri: ENDPOINT,
       form: {
-          title: 'Hello World'
-      },
-      body: {
-          some: 'payload'
+          action: action,
+          [action] : updateState
       },
       resolveWithFullResponse: true,
       json: true
     };
+  console.log(JSON.stringify(options));  
   return rp(options);
 };
 
 
-JarvasWebdesignHelper.prototype.formatAirportStatus = function(airportStatus) {
-  var weather = _.template('The current weather conditions are ${weather}, ${temp} and wind ${wind}.')({
-      weather: airportStatus.weather.weather,
-      temp: airportStatus.weather.temp,
-      wind: airportStatus.weather.wind
+JarvasWebdesignHelper.prototype.formatAirportStatus = function(isFailed, action, prevState, updateState) {
+  var weather = _.template('Updated ${action} to ${updateState}. Let me know anything else you want to change')({
+      action: action,
+      updateState: updateState
     });
-  if (airportStatus.delay === 'true') {
+  if (isFailed === 'true') {
       var template = _.template('There is currently a delay for ${airport}. ' +
                                      'The average delay time is ${delay_time}. ' +
                                      'Delay is because of the following: ${delay_reason}. ${weather}');
